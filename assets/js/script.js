49a7770c66e118bf831e926dfa71c698170cccf8
jh871 <jennyh871@gmail.com>
2023-02-08 00:13:33 +0000

    added moment.js and cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
 let searchBtn = $("#search-button");
 let searchInput = $("#search-input");
 let todayWeather = $("#today")
+let today = moment().format("D/MM/YYYY");
+
+
+let day1 = moment().add(1, "days").format("D/MM/YYYY");
+let day2 = moment().add(2, "days").format("D/MM/YYYY");
+let day3 = moment().add(3, "days").format("D/MM/YYYY");
+let day4 = moment().add(4, "days").format("D/MM/YYYY");
+let day5 = moment().add(5, "days").format("D/MM/YYYY");
+console.log(day1);
+console.log(day2);
+console.log(day3);
+console.log(day4);
+console.log(day5);
+
+
+
+
+
+
 
 searchBtn.on("click", function(event) {
 
     event.preventDefault();
 // cityName = searchInput.val
+
+
+
+
 
 let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=51.5073219&lon=-0.1276474&appid=24b0bccae4dbb8bd3aef5fad1d1c5cf5"
 
@@ -26,33 +49,57 @@ $.ajax({
     let humidity =  response.list[0].main.humidity;
     
     const weatherToday = $("<div>")
-
-
-    let todayHeader = $("<h3>");
-    todayHeader.html(cityName + " ("+ todaysDate + ") ") //+ icon
-    weatherToday.append(todayHeader);
-    todayHeader.attr("id", "todayHeader")
-
-    let todayTemp = $("<p>");
-    todayTemp.text("Temp: " + celsius +  "°c");
-    weatherToday.append(todayTemp);
-
-    let todayWind = $("<p>");
-    todayWind.text("Wind speed: " + wind + "KPH");
-    weatherToday.append(todayWind);
-
-
-    let todayHumidity= $("<p>");
-    todayHumidity.text("Humidity: " + humidity + "%");
-    weatherToday.append(todayHumidity);
-
-
+        let todayHeader = $("<h3>");
+        todayHeader.html(cityName + " ("+ today + ") ") //+ icon
+        weatherToday.append(todayHeader);
+        todayHeader.attr("id", "todayHeader")
+        let todayTemp = $("<p>");
+        todayTemp.text("Temp: " + celsius +  "°c");
+        weatherToday.append(todayTemp);
+        let todayWind = $("<p>");
+        todayWind.text("Wind speed: " + wind + "KPH");
+        weatherToday.append(todayWind);
+        let todayHumidity= $("<p>");
+        todayHumidity.text("Humidity: " + humidity + "%");
+        weatherToday.append(todayHumidity);
     todayWeather.append(weatherToday);
+
 
 }
 );
 })
+let forecast = $("#forecast");
 
+const days = [1, 2, 3, 4, 5]
+for (i = 0; i < days.length; i++) {
+    let forecastCard = $("<div>") //add class = card, width: 18rem
+        let cardBody = $("<div>") //add class card-body
+            let cardTitle = $("<h5>") //add class = card-title
+            //let cardIcon = $("<img>")
+            let cardTemp =  $("<p>"); // class = card.text
+            let cardWind =  $("<p>")
+            let cardHumidity =  $("<p>")
+
+            cardTitle.text("Date")
+            cardTemp.text("Temp: ")
+            cardWind.text("Wind: ")
+            cardHumidity.text("Humidity: ");
+
+
+
+
+
+            cardBody.append(cardTitle);
+            // cardBody.append(cardIcon)
+            cardBody.append(cardTemp);
+            cardBody.append(cardTemp);
+            cardBody.append(cardHumidity);
+        forecastCard.append(cardBody);
+    forecast.append(forecastCard);
+
+
+
+}           
 /*for hero weather:
 city (date) icon
 temp to fixed 2
